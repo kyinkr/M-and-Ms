350f9be584d65b553f8aa7baca5c6d8aefb18799
kyinkr <dhwlals@hansei.ac.kr>
2025-12-31 03:21:56 +0900

    Create Move Control.cs

--- M&Ms/Assets/Script/Move Control.cs
+++ M&Ms/Assets/Script/Move Control.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem.LowLevel;
 
 public class MoveControl: MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody rb;
 
+    private Vector3 movement;
     // Update is called once per frame
     void Update()
     {
-        
+        //좌우 이동
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
     }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position +  movement * moveSpeed * Time.fixedDeltaTime);
+    }     
 }
