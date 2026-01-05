c8f112de2f9fbb62594819917de98f9bafad580f
kyinkr <o01071833128@gmail.com>
2026-01-05 20:00:51 +0900

    Player MoveControl2

--- M&Ms/Assets/Script/Move Control2.cs
+++ M&Ms/Assets/Script/Move Control2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem.LowLevel;
 
-public class MoveControl: MonoBehaviour
+public class MoveControl2 : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody rb;
@@ -12,30 +12,30 @@ public class MoveControl: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Ï¢åÏö∞ Ïù¥Îèô
-        if (Input.GetKey(KeyCode.LeftArrow))
+        //¡¬øÏ ¿Ãµø
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
         }
 
     }
-   
+
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position +  movement * moveSpeed * Time.fixedDeltaTime);
-    }     
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
 }
